ccbd5b2cd81327b683fa56ec8ccb000c7f419114
EC2 Default User <ec2-user@ip-172-31-11-114.us-east-2.compute.internal>
2018-10-27 21:35:38 +0000

    Add destory action for Movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,6 +29,13 @@ class MoviesController < ApplicationController
         redirect_to movie_path(@movie)
     end
 
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
+    
     private
     def movie_params
         params.require(:movie).permit(:title, :rating, :release_date)
