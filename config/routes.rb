59920031a3af363d725f51e5006ec695d02cf369
EC2 Default User <ec2-user@ip-172-31-11-114.us-east-2.compute.internal>
2018-11-05 16:23:16 +0000

    Add routes for connect to Twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
     resources :movies
     root :to => redirect('/movies')
+    
+    get  'auth/:provider/callback' => 'sessions#create'
+    post 'logout' => 'sessions#destroy'
+    get  'auth/failure' => 'sessions#failure'
+    get  'auth/twitter', :as => 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
