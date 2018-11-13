22e59f043d6dc799d88eb726643cb104ee096ae4
EC2 Default User <ec2-user@ip-172-31-24-13.us-east-2.compute.internal>
2018-11-13 21:58:07 +0000

    Add search_tmdb to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     post 'logout' => 'sessions#destroy'
     get  'auth/failure' => 'sessions#failure'
     get  'auth/twitter', :as => 'login'
+    
+    post '/movies/search_tmdb'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
